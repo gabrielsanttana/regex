69732ab1c7f11a921f53fab781fe0199c57c8b1a
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-03-13 18:03:44 -0300

    Create CEP input mask

--- src/masks/masks.js
+++ src/masks/masks.js
@@ -24,6 +24,13 @@ const masks = {
       .replace(/(\d{2})(\d)/, '($1) $2')
       .replace(/(\d{5})(\d)/, '$1-$2')
       .replace(/(-\d{4})\d/, '$1')
+  },
+
+  cep(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{3})\d/, '$1')
   }
 };
 
