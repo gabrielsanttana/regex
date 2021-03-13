ee1f7013624288ec308db1a4172b6c76ea92a54b
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-03-13 17:53:00 -0300

    Create CNPJ input mask

--- src/masks/masks.js
+++ src/masks/masks.js
@@ -7,6 +7,16 @@ const masks = {
       .replace(/(\d{3})([0-9a-zA-Z]{1,2})/, "$1-$2")
       .replace(/(-\d{2})\d+?$/, '$1')
   },
+
+  cnpj(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1/$2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+  }
 };
 
 document.querySelectorAll('input').forEach((input) => {
