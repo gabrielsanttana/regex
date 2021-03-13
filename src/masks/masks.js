2e69ec0b011248dc377c9d395dbafa43ba093797
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-03-13 17:41:27 -0300

    Create CPF input mask

---
+++ src/masks/masks.js
@@ -0,0 +1,22 @@
+const masks = {
+  cpf(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})([0-9a-zA-Z]{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, '$1')
+  },
+};
+
+document.querySelectorAll('input').forEach((input) => {
+  const field = input.dataset.js;
+
+  input.addEventListener(
+    'input',
+    (event) => {
+      event.target.value = masks[field](event.target.value);
+    },
+    false
+  );
+});
