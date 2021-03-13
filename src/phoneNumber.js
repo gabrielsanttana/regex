702982d8721d701220695f3f8d31a9b95dddd1a3
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-03-13 16:47:06 -0300

    Add RegEx to select any phone number in american pattern

---
+++ src/phoneNumber.js
@@ -0,0 +1,15 @@
+//Select any phone number in american pattern
+
+function replacePhoneNumber(string) {
+  return string.replace(
+    /(?:(\+[0-9]{1,3})[ -])?\(?(\d{3})\)?[ -\.]?(\d{3})[ -\.]?(\d{4})/gm,
+    "phoneNumber"
+  );
+}
+
+console.log(replacePhoneNumber("1234567890")); // phoneNumber
+console.log(replacePhoneNumber("123-456-7890")); // phoneNumber
+console.log(replacePhoneNumber("123.456.7890")); // phoneNumber
+console.log(replacePhoneNumber("123 456 7890")); // phoneNumber
+console.log(replacePhoneNumber("(123) 456 7890")); // phoneNumber
+console.log(replacePhoneNumber("+1 (123) 456 7890")); // phoneNumber
