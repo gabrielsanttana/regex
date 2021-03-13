5d7f2bf0816148d58fb57e35ec578d6ee0caf924
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-03-13 18:23:45 -0300

    Create PIS input mask

--- src/masks/masks.js
+++ src/masks/masks.js
@@ -31,6 +31,15 @@ const masks = {
       .replace(/\D/g, '')
       .replace(/(\d{5})(\d)/, '$1-$2')
       .replace(/(-\d{3})\d/, '$1')
+  },
+
+  pis(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{5})(\d)/, '$1.$2')
+      .replace(/(\d{5})\.(\d{2})(\d)/, '$1.$2-$3')
+      .replace(/(-\d{1})\d/, '$1')
   }
 };
 
